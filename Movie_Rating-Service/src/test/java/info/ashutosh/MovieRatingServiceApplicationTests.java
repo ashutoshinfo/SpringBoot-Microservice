afdf918907977e462951f31b0dcecc4cb0b15ee3
Ashutosh <ashutoshpatel@yahoo.in>
2023-10-16 02:20:28 +0530

    Feat: Microservice

--- Movie_Rating-Service/src/test/java/info/ashutosh/MovieRatingServiceApplicationTests.java
+++ Movie_Rating-Service/src/test/java/info/ashutosh/MovieRatingServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package info.ashutosh;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MovieRatingServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package info.ashutosh;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MovieRatingServiceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
